66a4eb49baee5687766d36c3f399386ae72e48ca
Matthew Wahner <mattwahner@gmail.com>
2024-11-29 22:09:04 -0600

    diagnostic open float keybind

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -31,6 +31,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
 		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
 		vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+		vim.keymap.set('n', 'gv', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
 		vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
 		vim.keymap.set({'n', 'x'}, '<leader>lf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
 	end,

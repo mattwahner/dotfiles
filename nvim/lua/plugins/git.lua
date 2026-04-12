b982c5944231ddac7a770cf3eda8ffdcb6694161
Matthew Wahner <mattwahner@gmail.com>
2026-04-12 11:45:58 -0500

    Add neogit

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+
+			-- Only one of these is needed.
+			"sindrets/diffview.nvim", -- optional
+
+			-- For a custom log pager
+			"m00qek/baleia.nvim", -- optional
+
+			-- Only one of these is needed.
+			"folke/snacks.nvim", -- optional
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		},
+		opts = {
+			kind = "floating",
+		},
+	},
+}

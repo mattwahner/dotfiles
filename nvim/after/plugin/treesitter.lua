fecb3e7b8bca3623b0eb59014509bc0f14789f9c
Matthew Wahner <mattwahner@gmail.com>
2026-04-11 20:21:17 -0500

    treesitter updates

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,11 +1,3 @@
 
-require('nvim-treesitter.configs').setup({
-	sync_install = false,
-	ensure_installed = { "javascript", "typescript", "c", "lua", "rust" },
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-})
+require('nvim-treesitter').install({ "javascript", "typescript", "c", "lua", "rust" })
 

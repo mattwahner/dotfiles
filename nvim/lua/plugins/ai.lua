28c97834fcf48ca8fd1b30f1dfc8d950db95919c
Matthew Wahner <mattwahner@gmail.com>
2026-04-13 08:52:09 -0500

    avante changes

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,5 +2,18 @@ return {
 	{
 		"yetone/avante.nvim",
 		lazy = false,
+		opts = {
+			selector = {
+				provider = "snacks",
+			},
+			input = {
+				provider = "snacks",
+			},
+		},
+		keys = {
+			{ mode = { "n", "x" }, "<leader>aa", "<cmd>AvanteAsk<cr>", desc = "Avante Ask" },
+			{ mode = { "n", "x" }, "<leader>an", "<cmd>AvanteChatNew<cr>", desc = "Avante Ask New Chat" },
+			{ mode = { "n", "x" }, "<leader>ae", "<cmd>AvanteEdit<cr>", desc = "Avante Edit" },
+		},
 	},
 }
